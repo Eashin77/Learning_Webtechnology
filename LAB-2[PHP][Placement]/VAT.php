7397d70a3288b00c67a641927e332ebcf7791f15
Eashin77 <eashinjoy20gmail.com>
2021-09-27 21:29:57 +0600

    LAB-2[PHP][Placement] added

---
+++ LAB-2[PHP][Placement]/VAT.php
@@ -0,0 +1,18 @@
+<?php
+
+
+$vat = 15;
+
+
+$priceExcludingVat = 500;
+
+
+$vatToPay = ($priceExcludingVat / 100) * $vat;
+
+
+$totalPrice = $priceExcludingVat + $vatToPay;
+
+
+echo number_format($totalPrice);
+
+?>
